b35de424ffaefe62c8b70a0b913c9feb3f9cc676
Kirill Uksusov <uksusoff@yandex.ru>
2020-03-17 23:49:31 +0400

    Dependencies updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,9 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js('resources/assets/scripts/app.js', 'public/scripts').version();
 
-mix.js('resources/assets/scripts/app.js', 'public/scripts')
-    .version();
-mix.sass('resources/assets/styles/app.scss', 'public/styles')
-    .version();
+mix.sass('resources/assets/styles/app.scss', 'public/styles').version();
+
 mix.copy('node_modules/font-awesome/fonts/*.*', 'public/fonts');
 
 if (mix.inProduction()) {
